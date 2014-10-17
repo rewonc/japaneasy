ef314d6830298f94f8e66865a76f7f19a1e3a79c
rewonc <rewonc@gmail.com>
2014-10-17 13:45:00 -0700

    adding tests for initialization

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
-var dic = {}
+var dic = function(config){
+  var dictionary;
+  //set config varaibles
+  if (config && config.dictionary){
+    if (typeof config.dictionary != "string") throw new Error("Dictionary input must be a string");
+    if (!mapDictionary(config.dictionary)) throw new Error("Invalid parameter for dictionary")
+    dictionary = mapDictionary(config.dictionary);
+  }
 
-dic.search = function(text){
-  return('hello, world');
+
+  return search
+
+  function search (text){
+    if(text === "") {throw new Error("Input string cannot be blank")}
+    if(text === null || text === undefined) {throw "You must provide input that is not null or undefined"}
+    if(typeof text != "string") {throw "Input must be a string"}
+
+    return('hello, world');
+  }
+
+  function mapDictionary(key){
+    var map = {
+      "edict": "1",
+      "enamdict": "2",
+      "computing": "3",
+      "life-science": "4",
+      "legal": "5",
+      "finance": "6",
+      "buddhism": "7",
+      "misc": "8",
+      "gloss": "9",
+      "sci-eng": "A",
+      "linguistics": "B",
+      "river-water": "C",
+      "auto": "D",
+      "jap-word-net": "E",
+      "wip": "F",
+      "german": "G",
+      "french": "H",
+      "russian": "I",
+      "swedish": "J",
+      "hungarian": "K",
+      "spanish": "L",
+      "dutch": "M",
+      "slovenian": "N",
+      "italian": "O",
+      "untranslated": "P",
+      "combined-jap-eng": "Q",
+      "expanded-glossing": "R"
+    }
+    return map[key]
+  }
+
 }
+
+
 
 if (typeof module !== 'undefined') {
   module.exports = dic;
--- test/test.js
+++ test/test.js
@@ -16,31 +16,96 @@ describe("Initialization", function(){
     var a = new Dictionary({});
     expect(a).is.a("function");
   });
-  
-  //TODO: test for valid configuration options
+  var test = function(params){
+    return new Dictionary(params);
+  }
 
+  it("returns a function for valid dict entries", function(){
+    expect(test({dictionary: "edict"})).to.be.a('function');
+    expect(test({dictionary: "enamdict"})).to.be.a('function');
+    expect(test({dictionary: "computing"})).to.be.a('function');
+    expect(test({dictionary: "life-science"})).to.be.a('function');
+    expect(test({dictionary: "legal"})).to.be.a('function');
+    expect(test({dictionary: "finance"})).to.be.a('function');
+    expect(test({dictionary: "buddhism"})).to.be.a('function');
+    expect(test({dictionary: "glossing"})).to.be.a('function');
+    expect(test({dictionary: "german"})).to.be.a('function');
+    expect(test({dictionary: "french"})).to.be.a('function');
+    expect(test({dictionary: "dutch"})).to.be.a('function');
+  });
+
+  it("throws an error when initialized with invalid parameters for dictionary", function(){
+    expect(function(){test({dictionary: "blah"})}).to.throw('Invalid parameter for dictionary');
+    expect(function(){test({dictionary: "english"})}).to.throw('Invalid parameter for dictionary');
+    expect(function(){test({dictionary: "japanese"})}).to.throw('Invalid parameter for dictionary');
+    expect(function(){test({dictionary: ""})}).to.not.throw('Invalid parameter for dictionary');
+    expect(function(){test({dictionary: "blah"})}).to.throw('Invalid parameter for dictionary');
+    expect(function(){test({dictionary: {hello: "world"}})}).to.throw('Dictionary input must be a string');
+    expect(function(){test({dictionary: 123})}).to.throw('Dictionary input must be a string');
+  });
+
+  it("returns a function for valid params for method", function(){
+    expect(test({method: "kanji"})).to.be.a('function');
+    expect(test({method: "glossing"})).to.be.a('function');
+    expect(test({method: "word"})).to.be.a('function');
+  });
+  
+  it("throws an error for invalid params for method", function(){  
+    expect(function(){test({method: "blah"})}).to.throw('Invalid parameter for method');
+    expect(function(){test({method: "etc"})}).to.throw('Invalid parameter for method');
+    expect(function(){test({method: 123})}).to.throw('Method input must be a string');
+  });
+
+  it("returns a function for valid params for encoding", function(){
+    expect(test({encoding: "UTF-8"})).to.be.a('function');
+    expect(test({encoding: "Shift-JIS"})).to.be.a('function');
+    expect(test({encoding: "EUC"})).to.be.a('function');
+    expect(test({encoding: "ISO-2022-JP"})).to.be.a('function');
+    expect(test({encoding: "UCS"})).to.be.a('function');
+  });
+  
+  it("throws an error for invalid params for encoding", function(){  
+    expect(function(){test({encoding: "blah"})}).to.throw('Invalid parameter for encoding');
+    expect(function(){test({encoding: "etc"})}).to.throw('Invalid parameter for encoding');
+    expect(function(){test({encoding: 123})}).to.throw('Encoding input must be a string');
+  });
+
+  it("returns a function for custom string inputs that are long enough", function(){
+    expect(test({custom: "ab2i"})).to.be.a('function');
+    expect(test({custom: "conni"})).to.be.a('function');
+    expect(test({custom: "balhh"})).to.be.a('function');
+  }); 
+
+  it("throws an error for nonstring custom inputs", function(){
+    expect(function(){test({custom: 123})}).to.throw('Custom input must be a string');
+    expect(function(){test({custom: "a"})}).to.throw('Custom input must be at least 4 characters long');
+  });  
+  
 });
 
 describe('Limits on querying', function() {
   it("throws an exception to blank string", function(){
     var a = new Dictionary();
-    expect(a('')).to.throw('blank input');
+    expect(function(){a('')}).to.throw('cannot be blank');
   });
   it("throws an exception to null, undefined, or no input", function(){
     var a = new Dictionary();
-    expect(a()).to.throw('must provide input that is not null or undefined');
-    expect(a(null)).to.throw('must provide input that is not null or undefined');
-    expect(a(undefined)).to.throw('must provide input that is not null or undefined');
+    expect(function(){a()}).to.throw('must provide input that is not null or undefined');
+    expect(function(){a(null)}).to.throw('must provide input that is not null or undefined');
+    expect(function(){a(undefined)}).to.throw('must provide input that is not null or undefined');
   });
   it("throws an exception to non string types", function(){
     var a = new Dictionary();
-    expect(a({hello: "world"})).to.throw('must be a string');
-    expect(a(12345.10)).to.throw('must be a string');
-    expect(a(["abc","def"])).to.throw('must be a string');
+    expect(function(){a({hello: "world"})}).to.throw('must be a string');
+    expect(function(){a(12345.10)}).to.throw('must be a string');
+    expect(function(){a(["abc","def"])}).to.throw('must be a string');
   })
 });
 
-//TODO: test for valid queries & expected responses, one for each section
+//TODO: stub out url tests -- spy and make sure the url is correct
+
+
+//TODO: stub out using nock or rewire~~make sure the response is parsed correctly
 
 
 //TODO: test for mirrors and switching of them
