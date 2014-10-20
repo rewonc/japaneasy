94a097ff5b1fb76af99ec1934c7f821b3d88c962
rewonc <rewonc@gmail.com>
2014-10-19 20:03:48 -0700

    added tests for glossing and science plus passes

--- test/test.js
+++ test/test.js
@@ -196,7 +196,7 @@ describe('Parsing responses', function(){
 
     expect(parse(testString)[0].english).to.be.instanceOf(Array);
     expect(parse(testString)[0].english[0]).to.equal("hello (e.g. on phone)");
-  })
+  });
 
   it("accurately parses the response when no response exists'", function(){
     //hack for multiline string
@@ -205,20 +205,38 @@ describe('Parsing responses', function(){
     expect(parse(testString)).to.be.instanceOf(Array);
     expect(parse(testString)[0]).to.equal("No items were found; try another query.");
 
-  })
+  });
 
-  it("accurately parses the response for glossing '今日は、世界！'", function(){
+  it("accurately parses the response for scientific search 'chemolithotroph', input english", function(){
     //hack for multiline string
-    var testString = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">\n<HTML>\n<HEAD><META http-equiv="Content-Type" content="text/html; charset=UTF-8"><TITLE>WWWJDIC: Word Display</TITLE>\n</HEAD><BODY>\n<p>\n<pre>\nもしもし /(int) (1) hello (e.g. on phone)/(2) excuse me! (when calling out to someone)/(P)/\n今日は [こんにちは(P);こんちは] /(int) (uk) (こんちは is col.) hello/good day (daytime greeting)/(P)/\nどうも /(int) (1) (abbr) (See どうも有難う) thanks/(adv) (2) much (thanks)/very (sorry)/quite (regret)/(3) quite/really/mostly/(4) somehow/(5) (in positive sense, esp. どうも〜しまう) (See どうしても) in spite of oneself/no matter how hard one may try (one is unable to) (with negative verb)/no matter how hard one may try not to (one ends up doing) (with positive verb, esp. -shimau)/(int) (6) greetings/hello/goodbye/(P)/\nハロー(P);ハロ /(n) (1) halo/(2) (ハロー only) hello/hallo/hullo/(3) (ハロー only) harrow/(P)/\n</pre>\n</BODY>\n</HTML>\n';
-    
-    expect(parse(testString)).to.be.instanceOf(Array);
-    expect(parse(testString)[0].japanese).to.equal("もしもし");
+    var testString = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">\n<HTML>\n<HEAD><META http-equiv="Content-Type" content="text/html; charset=UTF-8"><TITLE>WWWJDIC: Word Display</TITLE>\n</HEAD><BODY>\n<p>\n<pre>\n化学合成無機栄養生物 [かがくごうせいむきえいようせいぶつ] /(n) chemolithotroph/\n\n無機栄養細菌 [むきえいようさいきん] /(n) chemolithotroph/\n\n化学合成無機栄養グラム陰性細菌 [かがくごうせいむきえいようぐらむいんせいさいきん] /(np) Gram-negative chemolithotrophic bacteria/\n\n</pre>\n</BODY>\n</HTML>\n'
 
+    expect(parse(testString)).to.be.instanceOf(Array);
+    expect(parse(testString)[0].japanese).to.equal("化学合成無機栄養生物");
+    expect(parse(testString)[0].pronunciation).to.equal("かがくごうせいむきえいようせいぶつ");
     expect(parse(testString)[0].english).to.be.instanceOf(Array);
-    expect(parse(testString)[0].english[0]).to.equal("hello (e.g. on phone)");
-  })
+    expect(parse(testString)[0].english[0]).to.equal("chemolithotroph");
+  });
+
+  it("accurately parses the response for glossing search '飛べねぇ豚はただのブタだ'", function(){
+    //hack for multiline string
+    var testString = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">\n<HTML>\n<HEAD><META http-equiv="Content-Type" content="text/html; charset=UTF-8"><TITLE>WWWJDIC: Text/word translation display</TITLE>\n</HEAD><BODY>\n<br>\n<font color="blue">飛べ</font>ねぇ<FONT color="red">豚</FONT>はただの<FONT color="red">ブタ</FONT>だ。<br>\n<ul><li> Possible inflected verb or adjective: (potential or imperative)<br>飛ぶ : 飛ぶ(P); 跳ぶ(P); 翔ぶ(oK) 【とぶ】 \t(v5b,vi) (1) (esp. 飛ぶ) to fly; to soar; (2) (esp. 跳ぶ) to jump; to leap; to spring; to bound; to hop; (P); ED </li>\n<li> 豚 : 豚(P); 豕 【ぶた(P); ブタ】 \t(n) (1) pig (Sus scrofa domesticus); (2) (derog) fat person; (P);  【とん】 ; (n-pref,n) pig; pork; ED </li>\n<li> ブタ : 豚(P); 豕 【ぶた(P); ブタ】 \t(n) (1) pig (Sus scrofa domesticus); (2) (derog) fat person; (P); ED </li>\n</ul><br>\n</BODY>\n</HTML>\n';
+
+    expect(parse(testString)).to.be.instanceOf(Array);
+    expect(parse(testString)[0].japanese).to.equal("飛ぶ");
+    expect(parse(testString)[0].pronunciation).to.equal("とぶ");
+    expect(parse(testString)[0].english).to.be.instanceOf(Array);
+    expect(parse(testString)[0].english[0]).to.equal("(esp. 飛ぶ) to fly; to soar;");
+
+    expect(parse(testString)[2].japanese).to.equal("ブタ");
+    expect(parse(testString)[2].pronunciation).to.equal("ぶた(P); ブタ");
+    expect(parse(testString)[2].english).to.be.instanceOf(Array);
+    expect(parse(testString)[2].english[0]).to.equal("pig (Sus scrofa domesticus);");
+  });
 
 });
+
+
 //TODO: stub out using nock or rewire~~make sure the response is parsed correctly
 
 
